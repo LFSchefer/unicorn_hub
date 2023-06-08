c864e7bee46466ac15a333b04f947d31d6e1c30b
louis frederic schefer <lf.schefer@gmail.com>
2023-06-08 22:36:26 +0200

    wip map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -9,13 +9,34 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log("connected to map")
-    mapboxgl.accessToken = this.apiKeyValue
+      console.log(this.apiKeyValue)
+      console.log(this.markersValue)
+      console.log(this.markersValue[0].info_window_html)
 
+      mapboxgl.accessToken = this.apiKeyValue
 
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      this.map = new mapboxgl.Map({
+        container: this.element,
+        style: "mapbox://styles/mapbox/streets-v10"
+      })
+      this.#addMarkersToMap()
+      this.#fitMapToMarkers()
+
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      // const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        // .setPopup(popup)
+        .addTo(this.map)
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
